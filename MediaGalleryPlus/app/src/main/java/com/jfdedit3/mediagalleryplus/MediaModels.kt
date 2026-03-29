49a70597084427d07e65bdd977a1c3519faee8f7
z4trox <kylian.lefebvre60@gmail.com>
2026-03-29 09:34:11 +0200

    Add MediaGalleryPlus source files

---
+++ MediaGalleryPlus/app/src/main/java/com/jfdedit3/mediagalleryplus/MediaModels.kt
@@ -0,0 +1,24 @@
+package com.jfdedit3.mediagalleryplus
+
+import android.net.Uri
+
+data class MediaItemModel(
+    val id: Long,
+    val uri: Uri,
+    val name: String,
+    val type: MediaType,
+    val dateAddedSeconds: Long
+)
+
+enum class MediaType {
+    IMAGE,
+    VIDEO,
+    AUDIO
+}
+
+enum class MediaTab {
+    ALL,
+    IMAGE,
+    VIDEO,
+    AUDIO
+}
