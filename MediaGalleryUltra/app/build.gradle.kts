ab3fc47792fc90e20437044780d10bbb4100fb4f
z4trox <kylian.lefebvre60@gmail.com>
2026-06-17 00:26:47 +0200

    V3 upgrade: add DataStore + coroutines + lifecycle dependencies

--- MediaGalleryUltra/app/build.gradle.kts
+++ MediaGalleryUltra/app/build.gradle.kts
@@ -47,7 +47,14 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+
     implementation("androidx.media3:media3-exoplayer:1.4.1")
     implementation("androidx.media3:media3-ui:1.4.1")
+
     implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    // V3 upgrades
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
 }
